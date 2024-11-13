153fd3dff2d8a4489dcc66add171e1d5e4b72623
tbektenov <tagirbektenov@gmail.com>
2024-11-13 17:05:13 +0600

    text typing animation added

--- client/royaldegen/src/components/OutputText/OutputTextComponent.tsx
+++ client/royaldegen/src/components/OutputText/OutputTextComponent.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./OutputTextComponent.css";
 
 interface Props {
@@ -5,9 +6,27 @@ interface Props {
 }
 
 const OutputTextComponent = ({ text }: Props) => {
+  const [displayedText, setDisplayedText] = useState<string>("");
+
+  useEffect(() => {
+    let index = 0;
+    setDisplayedText("");
+
+    const interval = setInterval(() => {
+      if (index < text.length) {
+        setDisplayedText((prev) => prev + text.charAt(index));
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, [text]);
+
   return (
     <div className="output-text-container">
-      <p>{text || "Your generated text will appear here."}</p>
+      <p>{displayedText || "Your generated text will appear here."}</p>
     </div>
   );
 };
