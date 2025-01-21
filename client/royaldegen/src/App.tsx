4a726d1c5f198341468c38867f1e18c518524100
tbektenov <tagirbektenov@gmail.com>
2025-01-21 23:25:26 +0600

    Trying to make nginx work

--- client/royaldegen/src/App.tsx
+++ client/royaldegen/src/App.tsx
@@ -37,7 +37,7 @@ function App() {
     setLoading(true);
     try {
       const token = localStorage.getItem("accessToken");
-      const response = await fetch("/generate-text", {
+      const response = await fetch("/api/generate-text", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- server/kingdom1/main.py
+++ server/kingdom1/main.py
@@ -98,10 +98,10 @@ class UserSubscription(BaseModel):
     end_date: Optional[datetime] = None
     status: str = 'active'
 
-@app.get("/getter")
+@app.get("/api/getter")
 async def getter():
     return "Hello World!"
-@app.post("/register")
+@app.post("/api/register")
 async def register(user: User):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
@@ -127,7 +127,7 @@ async def register(user: User):
         conn.close()
     return {"message": "User registered successfully"}
 
-@app.post("/login")
+@app.post("/api/login")
 async def login(user: UserLogin):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
@@ -147,7 +147,7 @@ async def login(user: UserLogin):
         logging.error(f"Failed login attempt for username: {user.username}")
         raise HTTPException(status_code=400, detail="Invalid username or password")
 
-@app.post("/generate-text")
+@app.post("/api/generate-text")
 async def generate_text(request: TextRequest, authorization: Optional[str] = Header(None)):
     try:
         # Check if the authorization header is provided for JWT
@@ -252,7 +252,7 @@ def check_text_for_nsfw_words(text, nsfw_words):
             return False
     return True
 
-@app.post("/subscriptions/plans")
+@app.post("/api/subscriptions/plans")
 async def create_subscription_plan(plan: SubscriptionPlan):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
@@ -276,7 +276,7 @@ async def create_subscription_plan(plan: SubscriptionPlan):
         cursor.close()
         conn.close()
 
-@app.post("/subscriptions")
+@app.post("/api/subscriptions")
 async def subscribe_user(user_subscription: UserSubscription):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
@@ -314,7 +314,7 @@ async def subscribe_user(user_subscription: UserSubscription):
         cursor.close()
         conn.close()
 
-@app.get("/subscriptions/{user_id}")
+@app.get("/api/subscriptions/{user_id}")
 async def get_user_subscription(user_id: int):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
